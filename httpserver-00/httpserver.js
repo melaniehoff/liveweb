e4f82c6e95aafb9575bd75f4d4f8ca73a44e831a
melaniehoff <hoffmelanie@gmail.com>
2016-10-03 21:53:20 -0400

    Add files via upload

---
+++ httpserver-00/httpserver.js
@@ -0,0 +1,89 @@
+var http = require('http');
+
+var fs = require('fs'); // Using the filesystem module
+
+var url = require('url');
+
+function onRequest(req, res) {
+
+    var parsedUrl = url.parse(req.url);
+    console.log("The Request is: " + parsedUrl.pathname);
+
+    // res.writeHead(200, {'Content-Type': 'text/plain'});
+    // 	res.end('You requested' + parsedUrl.pathname);
+
+    // Read in the file they requested
+    fs.readFile(__dirname + parsedUrl.pathname,
+        // Callback function, called when reading is complete
+        function(err, data) {
+            // if there is an error
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading ' + parsedUrl.pathname);
+            }
+            // Otherwise, send the data, the contents of the file
+            res.writeHead(200);
+            res.end(data);
+        }
+    );
+}
+
+// Send a log message to the console
+// console.log("Got a request " + req.url);
+//}
+
+// Call the createServer method, passing in an anonymous callback function that will be called when a request is made
+var httpServer = http.createServer(onRequest);
+
+// Tell that server to listen on port 8080
+httpServer.listen(8081);
+
+console.log("server running");
+
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var socketio = require('socket.io');
+var io = socketio.listen(httpServer);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+	// We are given a websocket object in our function
+	function (socket) {
+
+		console.log("We have a new client: " + socket.id);
+
+		// When this user "send" from clientside javascript, we get a "message"
+		// client side: socket.send("the message");  or socket.emit('message', "the message");
+		socket.on('message',
+			// Run this function when a message is sent
+			function (data) {
+				console.log("message: " + data);
+
+				// Call "broadcast" to send it to all clients (except sender), this is equal to
+				// socket.broadcast.emit('message', data);
+				//socket.broadcast.send(data);
+
+				// To all clients, on io.sockets instead
+				io.sockets.emit('message', data);
+			}
+		);
+
+		// When this user emits, client side: socket.emit('otherevent',some data);
+		socket.on('otherevent', function(data) {
+			// Data comes in as whatever was sent, including objects
+			console.log("Received: 'otherevent' " + data);
+		});
+
+socket.on('drawing', function(foodata) {
+  console.log("foooooooodata");
+  io.socket.emit('drawing', foodata);
+
+});
+
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected");
+		});
+	}
+);
